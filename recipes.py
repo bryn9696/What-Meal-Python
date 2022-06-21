eee2dbcaecdbfb2899f0ae1983d7db89f0791b96
bryn9696 <bryn2009@live.co.uk>
2022-06-21 15:36:48 +0100

    all ingredients returned with no duplicates

--- recipes.py
+++ recipes.py
@@ -1,13 +1,18 @@
+import functools
 class Recipes:
-  
-  options = []
+
   def __init__(self):
-	  self.options = []
+    self.options = []
+    self.cheese_toasty = ['Cheese', 'Bread', 'Butter']
+    self.ham_sandwhich = ['Ham', 'Bread', 'Butter']
+
+  def all_ingredients(self):
+    all_ing = self.cheese_toasty + self.ham_sandwhich
+    all = list(dict.fromkeys(all_ing))
+    return all
 
   def cheese_toasty_dish(self, ingredients_list):
-    # options = []
-    cheese_toasty = ['Cheese', 'Bread', 'Butter']
     for i in ingredients_list:
-      if i in cheese_toasty:
-        self.options.append(f"Cheese Toasty: {', '.join(cheese_toasty)}")
+      if i in self.cheese_toasty:
+        self.options.append(f"Cheese Toasty: {', '.join(self.cheese_toasty)}")
     return self.options
--- recipes_test.py
+++ recipes_test.py
@@ -1,5 +1,10 @@
 from recipes import Recipes
 
 def test_cheese_toasty() -> None:
-  r = Recipes()
-  assert r.cheese_toasty_dish(["Cheese"]) == (["Cheese Toasty: Cheese, Bread, Butter"])
+  assert Recipes().cheese_toasty_dish(["Cheese"]) == (["Cheese Toasty: Cheese, Bread, Butter"])
+
+def test_all_ingredients() -> None:
+  assert Recipes().all_ingredients == (['Cheese', 'Bread', 'Butter', 'Ham'])
+
+# def test_all_ingredients() -> None:
+#   assert Recipes().all_ingredients() == (['Cheese', 'Bread', 'Butter', 'Ham'])
