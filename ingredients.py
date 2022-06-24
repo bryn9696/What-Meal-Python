0762d2a5e5f2f04cb3d634db4ac68cacbb6e4fe7
bryn9696 <bryn2009@live.co.uk>
2022-06-24 15:06:42 +0100

    basics of dictionary function with test passing

--- ingredients.py
+++ ingredients.py
@@ -2,12 +2,34 @@ from recipes import Recipes
 
 class Ingredients:
   def ingredients(ingredient_list):
-    ing_list = []
-    
+    ing_list = []   
     for ing in ingredient_list:
       ing_string = str(ing).lower().capitalize()
       ing_list.append(ing_string)
       return ing_list
+
+  def dictionary(ingredients_list):
+    recipes = Recipes().all_ingredients()
+    spell_check = recipes
+    print(spell_check)
+    for x in spell_check:
+      i = 0
+      if ingredients_list[i] in x:
+        ingredients_list.pop(i)
+        ingredients_list.append(str(spell_check[i]))
+      else:
+        print("HI")
+      i += 1
+    print(ingredients_list)
+    return ingredients_list
+    # for ing in spell_check:
+    #   i = 0
+    #   if ing.chars().sort == ingredients_list[i].chars().sort():
+    #     ingredients_list.delete(ingredients_list[i])
+    #     ingredients_list << ing
+    #   i += 1
+    # print(recipes)
+    # ingredients_list.uniq.sort
 
   def choice(ingredients_list):
     Recipes.new.choices(ingredients_list)
--- ingredients_test.py
+++ ingredients_test.py
@@ -2,3 +2,6 @@ from ingredients import Ingredients
 
 def test_ingredients() -> None:
   assert Ingredients.ingredients(["HAM"]) == (["Ham"])
+
+def test_dictionary() -> None:
+  assert Ingredients.dictionary(['Cheese']) == (["Cheese"])
